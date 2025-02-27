aa3b206d5a8647c06d6d696d58ebbcfd2c422427
pablomedina7 <pablomedinasantos@hotmail.com>
2025-02-27 11:39:13 -0300

    cambios visuales version beta

--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -1,12 +1,12 @@
-// // main.js
-// import { createApp } from 'vue';
-// import App from './App.vue';
-// import router from './router';
-// import { createPinia } from 'pinia';
-// import './assets/style.css';
+// main.js
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createPinia } from 'pinia';
+import './assets/style.css';
 
-// const app = createApp(App);
-// app.use(router);
-// app.use(createPinia());
-// app.mount('#app');
+const app = createApp(App);
+app.use(router);
+app.use(createPinia());
+app.mount('#app');
 
