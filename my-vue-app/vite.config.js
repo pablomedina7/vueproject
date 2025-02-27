c3f20a5a743a1cffb8786cda9eb91d9fb3a6f8dc
pablomedina7 <pablomedinasantos@hotmail.com>
2025-02-27 12:19:22 -0300

    cambios visuales version beta

--- my-vue-app/vite.config.js
+++ my-vue-app/vite.config.js
@@ -1,20 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
-})
+  build: {
+    assetsDir: 'assets'
+  },
+  server: {
+    base: '/'
+  }
+});
