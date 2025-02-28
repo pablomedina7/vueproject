848816af797640cba379804cb13ccc764412ec7a
pablomedina7 <pablomedinasantos@hotmail.com>
2025-02-28 17:01:51 -0300

    Cambios en rutas de navegacion

--- my-vue-app/vite.config.js
+++ my-vue-app/vite.config.js
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'public/index.html',
+        sobreNosotros: 'public/sobre_nosotros.html',
+        contacto: 'public/contacto.html'
+      }
     }
   },
-  build: {
-    assetsDir: 'assets'
-  },
   server: {
-    base: '/'
-  }
+    base: '/',
+  },
 });
